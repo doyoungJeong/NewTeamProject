efd827417491e3952abc58581be7e7cd05d29fb8
yjkim9306 <rladpwl9306@naver.com>
2015-09-04 19:49:37 +0900

    membercontroller

--- NewTeamProject/src/main/java/com/mycompany/myapp/controller/MemberController.java
+++ NewTeamProject/src/main/java/com/mycompany/myapp/controller/MemberController.java
@@ -3,10 +3,12 @@ package com.mycompany.myapp.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mycompany.myapp.service.MemberService;
 
+@Controller
 public class MemberController {
 private int a;
 private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
